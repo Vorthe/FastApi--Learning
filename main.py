c4c1847c6886327e8e8c35bf953b22f151d8afd4
Vorthe <nimasalimi19950404@gmail.com>
2026-06-18 19:55:11 +0330

    a first commit in GitHub

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
-    return {"status": "Vorthe is active", "message": "API is running perfectly"}
+    return {"status": "Nima is active", "message": "Hello World"}
