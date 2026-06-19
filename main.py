b6800683991d539f8e9d59d64710b332e0973745
Vorthe <nimasalimi19950404@gmail.com>
2026-06-19 20:16:34 +0330

    feat: Add book endpoints with dynamic routing

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
 
 
-@app.get("/")
-def home():
-    return {"status": "Nima is active", "message": "Hello World"}
+# @app.get("/")
+# def home():
+#     return {"status": "Nima is active", "message": "Hello World"}
+
+
+
+
+
+
+
+# app = FastAPI()
+
+# @app.get("/api-endpoint")
+# async def first_api():
+#     return {"message": "Hello Nima"}
+
+
+# async def first_api():
+#     return {"message": "Hello Nima"}
