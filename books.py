da7d6eeeb2867276439be5746c7c77a422baa1a3
Vorthe <nimasalimi19950404@gmail.com>
2026-06-20 13:36:56 +0330

    change function

--- books.py
+++ books.py
@@ -29,7 +29,7 @@ async def read_all_books():
 
 
 @app.get("/books/{book_title}")
-async def read_all_books(book_title: str):
+async def read_book(book_title: str):
     for book in BOOKS:
         if book.get("title").casefold() == book_title.casefold():
             return book
