01d9d9873fe9e10976c8704ae3135cbe9845d1dc
Vorthe <nimasalimi19950404@gmail.com>
2026-06-22 23:52:01 +0330

    Update Basic CRUD

--- books2.py
+++ books2.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Body
+from typing import Optional
+from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -19,6 +20,25 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(description="ID is not needed on create", default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "codingwithroby",
+                "description": "A new description of a book",
+                "rating": 5,
+            }
+        }
+    }
+
+
 BOOKS = [
     Book(1, "Computer Science Pro", "codingwithroby", "a very nice book!", 5),
     Book(2, "BE Fast with FastAPI", "codingwithroby", "a great book!", 5),
@@ -29,20 +49,53 @@ BOOKS = [
 ]
 
 
-class BookRequest(BaseModel):
-    id: int
-    title: str = Field(min_length=3)
-    author: str = Field(min_length=1)
-    description: str = Field(min_length=1, max_length=100)
-    rating: int = Field(gt=0, lt=5)
-
-
 @app.get("/books")
 async def read_all_books():
     return BOOKS
 
 
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book_id == book_id:
+            return book
+
+
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int):
+    book_to_return = []
+    for book in BOOKS:
+        if book_rating == book_rating:
+            book_to_return.append(book)
+            return book_to_return
+
+
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    # if len(BOOKS) > 0:
+    #     book.id = BOOKS[-1].id + 1
+    # else:
+    #     book.id = 1
+
+    return book
+
+
+@app.put("/book/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
