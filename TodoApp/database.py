e19967fb9606d06c07e9ee8b2d200a54f71a669b
Vorthe <nimasalimi19950404@gmail.com>
2026-06-27 23:55:07 +0330

    DataBase CRUD Complete

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -6,5 +6,5 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflash=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
