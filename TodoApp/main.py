0bfc1d8cdf734d42f91241aefb377a2782a2ef77
Vorthe <nimasalimi19950404@gmail.com>
2026-06-29 23:56:35 +0330

    add authentication and scale project architecture

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -6,12 +6,14 @@ import models
 from models import Todos
 from database import engine, SessionLocal
 from starlette import status
+from routers import auth
 
 app = FastAPI()
 
 
 models.Base.metadata.create_all(bind=engine)
 
+app.include_router(auth.router)
 
 def get_db():
     db = SessionLocal()
