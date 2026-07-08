b2ed08f4fe2daeb0dfa2b266b8e94154eb2ad200
Vorthe <nimasalimi19950404@gmail.com>
2026-07-08 23:57:09 +0330

    learn: pytest and testing

---
+++ TodoApp/test/test_todos.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
+from ..database import Base
+from ..main import app
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def ovverride_get_current_user():
+    return {"username": "nima", "id": 1, "user_role": "admin"}
+
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
